25d898fc07529db31131917b9b8e49eb6f1a0ccd
chaeheonjeong <116990111+chaeheonjeong@users.noreply.github.com>
2025-05-10 21:45:26 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 0.38 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/README.md
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 전력망을 둘로 나누기 - 86971 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/86971) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/86971?language=javascript) 
 
 ### 성능 요약
 
-메모리: 80.4 MB, 시간: 0.55 ms
+메모리: 33.6 MB, 시간: 0.38 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 19일 22:49:41
+2025년 05월 10일 21:45:23
 
 ### 문제 설명
 
---
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.js
@@ -0,0 +1,35 @@
+let visited = [];
+let tree = [];
+let N = 0;
+let answer = Number.MAX_SAFE_INTEGER;
+let cnt = 0;
+
+function dfs(now){
+    visited[now] = true;
+    
+    let sum = 0;
+    for(let x of tree[now]){
+        if(!visited[x]){
+            sum += dfs(x);
+        }
+    }
+    
+    sum++;
+    answer = Math.min(answer, Math.abs(sum - (N-sum)));
+    return sum;
+}
+
+function solution(n, wires) {
+    N = n;
+    visited = new Array(n).fill(false);
+    tree = Array.from({length: n}, () => []);
+    
+    for(let [a, b] of wires){
+        tree[a-1].push(b-1);
+        tree[b-1].push(a-1);
+    }
+    
+    dfs(0);
+    
+    return answer;
+}
